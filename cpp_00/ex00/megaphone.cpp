bc90715eb5ac6be20e3fe168448462edb922740f
howsetya <howsetya@googlemail.com>
2025-05-12 12:05:13 +0200

    refactor megaphone.cpp

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,15 +6,17 @@ int main(int argc, char *argv[])
 
 	if (argc == 1)
 	{
-		std :: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std :: endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (1);
 	}
-	str = &argv[1];
-	for (int i = 0; str[i]; i++)
+	else
 	{
-		for (int j = 0; str[i][j]; j++)
-			std :: cout << (char)(std::toupper(str[i][j]));
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)(std::toupper(argv[i][j]));
+		}
+		std::cout << std::endl;
+		return (0);
 	}
-	std :: cout << std :: endl;
-	return (0);
 }
