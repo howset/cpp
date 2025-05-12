232136d12bc42f3de25ba47fa7fe79b166bebf60
howsetya <howsetya@googlemail.com>
2025-05-12 17:19:24 +0200

    streamline megaphone

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
+#include <iostream> //already includes ostream: cout, endl
+#include <cctype> //toupper
 
 int main(int argc, char *argv[])
 {
-	char	**str;
-
-	if (argc == 1)
+	if (argc < 2)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (1);
@@ -15,6 +14,8 @@ int main(int argc, char *argv[])
 		{
 			for (int j = 0; argv[i][j]; j++)
 				std::cout << (char)(std::toupper(argv[i][j]));
+			if (i < argc - 1)
+				std::cout << ' ';
 		}
 		std::cout << std::endl;
 		return (0);
