d92e1ed4140a7b50c1a28ee1e78721c0b1fca725
howsetya <howsetya@googlemail.com>
2025-06-26 19:52:02 +0200

    ex05

---
+++ cpp_01/ex05/Harl.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 19:07:26 by hsetyamu          #+#    #+#             */
+/*   Updated: 2025/06/26 19:09:36 by hsetyamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		void	complain(std::string level);
+};
+
+#endif
---
+++ cpp_01/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 17:01:18 by hsetyamu          #+#    #+#             */
+/*   Updated: 2025/06/26 19:51:04 by hsetyamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(int argc, char *argv[])
+{
+	Harl	harl;
+	std::string	level;
+
+	level = argv[1];
+	if (argc == 2)
+		harl.complain(level);
+	else
+		std::cout << "Unrecognized complain" << std::endl;
+}
