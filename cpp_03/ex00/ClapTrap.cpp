07966f42044c9f2b6d067e52dc7cfdd95ad4ef22
howsetya <howsetya@googlemail.com>
2025-07-03 15:12:22 +0200

    .

--- cpp_03/ex00/ClapTrap.cpp
+++ cpp_03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 15:41:57 by hsetyamu          #+#    #+#             */
-/*   Updated: 2025/07/02 16:25:32 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2025/07/02 18:40:47 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,86 @@
 ClapTrap::ClapTrap(std::string name)
 {
 	this->_name = name;
-	std::cout << "ClapTrap " << this->_name << " constructed" << std::endl;
-
+	std::cout << "ClapTrap " << this->_name << " constructed." << std::endl;
+	this->_hp = 10;
+	this->_ep = 10;
+	this->_ad = 1;
 }
 
 ClapTrap::~ClapTrap()
 {
-	std::cout << "ClapTrap " << this->_name << " destructed" << std::endl;
+	std::cout << "ClapTrap " << this->_name << " destructed." << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap &copy)
 {
-	std::cout << "Copy constructor called" << std::endl;
-	//_fpval = copy._fpval;
+	std::cout << "Copy constructor called." << std::endl;
+	*this = copy;
 }
 
 ClapTrap &ClapTrap::operator=(const ClapTrap &copy)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
-	//if (this != &copy) //best practice to check for self-assignment
-	//	_fpval = copy._fpval;
-	//return (*this);
+	std::cout << "Copy assignment operator called." << std::endl;
+	if (this != &copy) //best practice to check for self-assignment
+	{
+		this->_name = copy._name;
+		this->_hp = copy._hp;
+		this->_ep = copy._ep;
+		this->_ad = copy._ad;
+	}
+	return (*this);
 }
 
-void attack(const std::string& target)
+void ClapTrap::attack(const std::string& target)
 {
-	ClapTrap <name> attacks <target>, causing <damage> points of damage!
+	if (this->_hp <= 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " has no more hit points!" << std::endl;
+		return ;
+	}
+	if (this->_ep > 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " attacks " << target << " causing " << this->_ad  << " points of damage!"<< std::endl;
+		this->_ep--;
+	}
+	else
+		std::cout << "ClapTrap " << this->_name << " has no more energy points!" << std::endl;
+
 }
 
-void takeDamage(unsigned int amount)
-{}
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->_hp > 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " took " << amount << " point(s) of damage!" << std::endl;
+		this->_hp = this->_hp - amount;
+		if (this->_hp < 0)
+			this->_hp = 0;
+	}
+	else if (this->_hp <= 0)
+		std::cout << "ClapTrap " << this->_name << " has no more hit points!" << std::endl;
+}
 
-void beRepaired(unsigned int amount)
-{}
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_hp == 10)
+		std::cout << "ClapTrap " << this->_name << " is healthy." << std::endl;
+	if (this->_hp > 0 && this->_ep > 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " heals " << amount << " hit point(s)." << std::endl;
+		this->_hp = this->_hp + amount;
+		this->_ep--;
+	}
+	else if (this->_hp <= 0 || this->_ep <= 0)
+		std::cout << "ClapTrap " << this->_name << " can't do anything." << std::endl;
+}
+
+std::string const	&ClapTrap::getName(void) const
+{
+	return (this->_name);
+}
+
+int const	&ClapTrap::getHp(void) const
+{
+	return (this->_hp);
+}
--- cpp_03/ex00/ClapTrap.hpp
+++ cpp_03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 15:41:55 by hsetyamu          #+#    #+#             */
-/*   Updated: 2025/07/02 16:20:51 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2025/07/02 18:41:39 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,20 @@ class ClapTrap
 {
 private:
 	std::string	_name;
-	int			_hp = 10;
-	int			_ep = 10;
-	int			_ad = 0;
+	int			_hp;
+	int			_ep;
+	int			_ad;
 public:
 	ClapTrap(std::string name);
 	~ClapTrap();
 	ClapTrap(const ClapTrap &copy); //copy constr
 	ClapTrap	&operator=(const ClapTrap &copy); //assignment operator
 
-	void attack(const std::string& target);
-	void takeDamage(unsigned int amount);
-	void beRepaired(unsigned int amount);
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+
+	std::string const	&getName(void) const;
+	int const			&getHp(void) const;
 };
 
