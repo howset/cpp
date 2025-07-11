af27676f1d29e8dbfc56d4d9326306ff59048f56
howsetya <howsetya@googlemail.com>
2025-07-11 13:54:04 +0200

    Add notes and fix copy function in c5

--- cpp_05/ex00/Bureaucrat.cpp
+++ cpp_05/ex00/Bureaucrat.cpp
@@ -6,18 +6,37 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 19:29:43 by hsetyamu          #+#    #+#             */
-/*   Updated: 2025/07/09 19:29:44 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2025/07/11 13:52:32 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(): _name("default"), _grade(42) //??
+/*
+## **Best Practice Guidelines:**
+### **Always Use Initializer Lists For:**
+- Const members
+- Reference members  
+- Base classes
+- Member objects without default constructors
+### **Prefer Initializer Lists For:**
+- All other members (consistency and performance)
+- When no complex logic needed
+### **Body Assignment OK For:**
+- Complex initialization requiring validation
+- Conditional initialization logic
+- When value has to be computed first
+
+**Rule of Thumb:** Use initializer lists whenever possible, use body assignment
+only when you need the flexibility of procedural logic.
+*/
+
+Bureaucrat::Bureaucrat(): _name("default"), _grade(42)
 {
 	std::cout << CYA << "Bureaucrat " << this->_name << " constructed." << RES << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const std::string &name, int grade): _name(name) //??
+Bureaucrat::Bureaucrat(const std::string &name, int grade): _name(name)
 {
 	this->_grade = grade;
 	if (this->_grade < 1)
@@ -32,10 +51,10 @@ Bureaucrat::~Bureaucrat()
 	std::cout << CYA << "Bureaucrat " << this->_name << " destructed." << RES << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat &copy)
+Bureaucrat::Bureaucrat(const Bureaucrat &copy): _name(copy._name), _grade(copy._grade)
 {
 	std::cout << CYA << "Copy constructor called." << RES << std::endl;
-	*this = copy;
+	//*this = copy; //this can't assign to const _name, so use member initialization list
 }
 
 Bureaucrat &Bureaucrat::operator=(const Bureaucrat &copy)
