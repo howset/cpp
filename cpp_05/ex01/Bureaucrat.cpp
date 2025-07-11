6bf6672c56a5d669c789bf05b5265ae0c6763ddb
howsetya <howsetya@googlemail.com>
2025-07-11 15:53:56 +0200

    More comments in c5

--- cpp_05/ex01/Bureaucrat.cpp
+++ cpp_05/ex01/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 19:29:15 by hsetyamu          #+#    #+#             */
-/*   Updated: 2025/07/11 13:53:16 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2025/07/11 15:53:36 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 #include "Form.hpp"
 
 /*
-## **Best Practice Guidelines:**
-### **Always Use Initializer Lists For:**
+Best Practice Guidelines:
+Always Use Initializer Lists For:
 - Const members
 - Reference members  
 - Base classes
 - Member objects without default constructors
-### **Prefer Initializer Lists For:**
+Prefer Initializer Lists For:
 - All other members (consistency and performance)
 - When no complex logic needed
-### **Body Assignment OK For:**
+Body Assignment OK for:
 - Complex initialization requiring validation
 - Conditional initialization logic
 - When value has to be computed first
@@ -31,12 +31,22 @@
 **Rule of Thumb:** Use initializer lists whenever possible, use body assignment
 only when you need the flexibility of procedural logic.
 */
-
 Bureaucrat::Bureaucrat(): _name("default"), _grade(42)
 {
 	std::cout << CYA << "Bureaucrat " << this->_name << " constructed." << RES << std::endl;
 }
 
+/*
+Member Initialization List
+- Members are directly constructed with the provided values
+- Happens before the constructor body executes
+- Single step: construct with final value
+
+Body Assignment
+- Members are default constructed first
+- Then assigned new values in the constructor body
+- Two steps: default construct + assign
+*/
 Bureaucrat::Bureaucrat(const std::string &name, int grade): _name(name)
 {
 	this->_grade = grade;
