9a2818782706a9d0149343d85934e746e20a3537
howsetya <howsetya@googlemail.com>
2025-07-11 16:04:04 +0200

    More comments in c5

--- cpp_05/ex00/main.cpp
+++ cpp_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 14:23:27 by hsetyamu          #+#    #+#             */
-/*   Updated: 2025/07/09 15:46:11 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2025/07/11 16:01:35 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,22 @@
 
 #include <iostream>
 
+/*
+Exception handling is a mechanism to handle runtime errors gracefully instead of
+crashing the program. It uses three keywords: throw, try, and catch.
+- throw is used to raise (throw) an exception when an error occurs.
+	- Normal execution stops immediately
+	- Program looks for a matching catch block
+	- If no catch block found, program terminates
+- try defines a block of code that might throw exceptions (protected code block)
+	- Code inside try block executes normally
+	- If exception is thrown, execution jumps to matching catch block
+	- Code after the throw point doesn't execute
+- catch defines how to handle specific types of exceptions (handler)
+	- Catches any exception that inherits from std::exception
+	- e.what() returns the error message
+	- Program continues after the catch block
+*/
 int	main(void)
 {
 	std::cout << "#########################################" << std::endl;
