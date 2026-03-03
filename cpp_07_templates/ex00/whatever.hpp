1685a265188c22bdbc29fe455ba63fdc95961432
howsetya <howsetya@googlemail.com>
2026-03-03 17:18:06 +0100

    minor changes

--- cpp_07_templates/ex00/whatever.hpp
+++ cpp_07_templates/ex00/whatever.hpp
@@ -5,24 +5,21 @@
 //E.g. usign a sort() func for different data types. 
 // Template definitions need to be in header
 template <typename T>
-void swap(T &a, T &b)
-{
+void swap(T &a, T &b){
 	T tmp = a;
 	a = b;
 	b = tmp;
 }
 
 template <typename T>
-T max(const T &a, const T &b)
-{
+T max(const T &a, const T &b){
 	if (a > b)
 		return (a);
 	return (b);
 }
 
 template <typename T>
-T min(const T &a, const T &b)
-{
+T min(const T &a, const T &b){
 	if (a < b)
 		return (a);
 	return (b);
